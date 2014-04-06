144822b044b9a5006ff810c36efdd1208dd81ef0
Mike Helmick <mike.helmick@uc.edu>
2014-04-05 22:38:45 -0400

    small updates during talk

--- src/main/java/com/mikehelmick/lambda/SortingExample.java
+++ src/main/java/com/mikehelmick/lambda/SortingExample.java
@@ -12,8 +12,8 @@ public class SortingExample {
   public static void main(String[] args) {
     List<Person> data1 = DataLoader.loadData();
     // Create a copy, so we can sort both.
-    List<Person> data2 = Lists.newArrayList(data1);
-    
+    List<Person> data2 = DataLoader.loadData();
+
     System.out.println("Old school sorting");
     // Sorting w/o lambdas
     Collections.sort(data1,
@@ -34,7 +34,7 @@ public class SortingExample {
     // Sorting with lambdas
     // Still kind of ugly becuase the comparison condition isn't straight foward.
     Collections.sort(data2,
-        (p1, p2) -> 
+        (p1, p2) ->
             {
               int res = p1.getName().compareTo(p2.getName());
               if (res != 0) {
